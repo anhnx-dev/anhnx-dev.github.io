b4ce10da2269f7170054e72ab07385bff203bbec
anhnx-dev <anhnx26121992@gmail.com>
2018-08-20 15:01:31 +0700

    Fix js for Vnedutech

--- vnedutech/src/js/main.js
+++ vnedutech/src/js/main.js
@@ -1,87 +1,81 @@
-// Hero
-const banner = document.querySelector('.hero__banner');
-// const bannerImage = document.querySelector('.hero__banner img');
-const ACTIVE_ANIMATION_CLASS = 'active-animetion';
-window.addEventListener('load', () => {
-  banner.classList.add(ACTIVE_ANIMATION_CLASS);
+// Banner
+var banner = document.querySelector('.hero__banner');
+var ACTIVE_ANIMATION_CLASS = 'active-animetion';
+window.addEventListener('load', function () {
+  banner.classList.add(ACTIVE_ANIMATION_CLASS)
 });
 
-// Navbar
-const navbar = document.querySelector('.js-navbar');
-const DROPDOWN_CLASS = 'dropdown';
-const ACTIVE_DROPDOWN_CLASS = 'dropdown-active';
-const SUB_MENU_CLASS = 'sub-menu';
-const submenus = document.querySelectorAll('.js-navbar>ul li ul');
-
+// navbar
+var navbar = $('.js-navbar');
+var SUB_MENU_CLASS = 'sub-menu';
+var DROPDOWN_CLASS = 'dropdown';
+var ACTIVE_DROPDOWN_CLASS = 'dropdown-active';
 if (navbar) {
-  for (let i = 0; i < submenus.length; i++) {
-    const submenu = submenus[i];
-    submenu.parentNode.classList.add(DROPDOWN_CLASS);
-    submenu.classList.add(SUB_MENU_CLASS);
-  }
-  const dropdownButtons = document.querySelectorAll('.dropdown>a');
-  for (let i = 0; i < dropdownButtons.length; i++) {
-    const dropdownButton = dropdownButtons[i];
-    dropdownButton.removeAttribute('href');
-    dropdownButton.addEventListener('click', () => {
-      dropdownButton.parentNode.classList.toggle(ACTIVE_DROPDOWN_CLASS);
-    });
-    // return false;
-  }
+
+  var subMenu = $('.js-navbar>ul>li ul');
+  subMenu.addClass(SUB_MENU_CLASS);
+  subMenu.parent().addClass(DROPDOWN_CLASS);
+
+  var dropdown = $('.js-navbar .dropdown');
+  var dropdownButton = $('.js-navbar .dropdown>a');
+
+  dropdownButton.on('click', function () {
+    $(this).parent().toggleClass(ACTIVE_DROPDOWN_CLASS).siblings().removeClass(ACTIVE_DROPDOWN_CLASS);
+    return false;
+  });
 }
 
-
 // Menu Mobile
-const navTrigger = document.querySelector('.js-trigger');
-const slideoutOvarlay = document.querySelector('.js-slideout-overlay');
-const FIXED_CLASS = 'fixed-top';
-const body = document.body;
+var navTrigger = $('.js-trigger');
+var slideoutOvarlay = $('.js-slideout-overlay');
+var FIXED_CLASS = 'fixed-top';
+var body = $('body');
 
 if (navTrigger) {
-  navTrigger.addEventListener('click', () => {
-    body.classList.toggle(FIXED_CLASS)
+  navTrigger.on('click', function () {
+    body.toggleClass(FIXED_CLASS);
   });
-  slideoutOvarlay.addEventListener('click', () => {
-    body.classList.remove(FIXED_CLASS)
-  })
+  slideoutOvarlay.on('click', function () {
+    body.removeClass(FIXED_CLASS);
+  });
 }
 
 // accordion
-const ITEM_CLASS = 'js-accordion';
-const ACTIVE_CLASS = 'accordion-active';
-const HEADER_ITEM_CLASS = 'js-accordion-header';
-const BODY_ITEM_CLASS = 'js-accordion-body';
-const BODY_WRAPPER_CLASS = 'js-accordion-body-inner';
+var accordion = 'js-accordion';
 
-const addClass = (item) => item.classList.add(ACTIVE_CLASS);
-const removeClass = (item) => item.classList.remove(ACTIVE_CLASS);
-const hideContent = (content) => content.style.height = 0;
-const isActivated = (content, contentWarpper) => content.style.height = (contentWarpper.offsetHeight) + 'px';
+if (accordion) {
+  var accordionButton = $('.js-accordion h3');
+  accordionButton.on('click', function () {
+    $(this).parent().parent().toggleClass('active').siblings().removeClass('active');
+  });
+}
 
-const accordion = () => {
-  const items = document.getElementsByClassName(ITEM_CLASS);
-  const buttons = document.getElementsByClassName(HEADER_ITEM_CLASS);
-  const contents = document.getElementsByClassName(BODY_ITEM_CLASS);
-  const contentsWarpper = document.getElementsByClassName(BODY_WRAPPER_CLASS);
-  if (items) {
-    for (let i = 0; i < items.length; i++) {
-      buttons[i].addEventListener('click', () => {
-        if (items[i].classList[2] == ACTIVE_CLASS) {
-          removeClass(items[i]);
-          hideContent(contents[i]);
-        } else {
-          for (let i = 0; i < buttons.length; i++) {
-            removeClass(items[i]);
-            hideContent(contents[i]);
-          }
-          addClass(items[i]);
-          isActivated(
-            contents[i],
-            contentsWarpper[i]
-          );
+
+// countdown
+var COUNTDOWN_CLASS = $('.js-counter');
+
+if (COUNTDOWN_CLASS) {
+  COUNTDOWN_CLASS.appear();
+
+  $(document.body).on('appear', '.achievements', function (e, $affected) {
+    COUNTDOWN_CLASS.parent().addClass('active');
+    COUNTDOWN_CLASS.each(function () {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+        countNum: countTo
+      }, {
+        duration: 8000,
+        easing: 'linear',
+        step: function () {
+          $this.text(Math.floor(this.countNum));
+        },
+        complete: function () {
+          $this.text(this.countNum);
         }
       });
-    }
-  }
-};
-accordion();
+    });
+  });
+}
