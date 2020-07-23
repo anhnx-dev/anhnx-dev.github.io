a16d509fccab5bbb0d2ae0fa349288bf7d506288
Anh Nguyen <anhnx26121992@gmail.com>
2020-07-23 23:31:14 +0700

    updata v28

--- view/v28/src/main.js
+++ view/v28/src/main.js
@@ -24,3 +24,32 @@ if (accordion) {
     });
   }
 }
+
+// modal
+const modal = document.querySelectorAll('[data-modal]');
+if (modal) {
+  for (let i = 0; i < modal.length; i++) {
+    const button = modal[i];
+    const body = $('body');
+    const exit = $('.modal-exit');
+    const CLASS_ACTIVE_MODAL = 'modal-active';
+    const CLASS_ACTIVE_OVER = 'modal-over-body-active';
+    $(button).on('click', function () {
+      body.prepend('<div class="modal-over-body"></div>');
+      const over = $('.modal-over-body');
+      const e = button.getAttribute('data-modal');
+      $(e).addClass(CLASS_ACTIVE_MODAL);
+      $(over).addClass(CLASS_ACTIVE_OVER);
+      over.on('click', function () {
+        $(e).removeClass(CLASS_ACTIVE_MODAL);
+        $(over).removeClass(CLASS_ACTIVE_OVER);
+        over.remove();
+      });
+      exit.on('click', function () {
+        $(e).removeClass(CLASS_ACTIVE_MODAL);
+        $(over).removeClass(CLASS_ACTIVE_OVER);
+        over.remove();
+      });
+    });
+  }
+}
