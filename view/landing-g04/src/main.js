03bf0f51b1c80e6afc8fe6f9550506fdeae42627
anhnx <anhnx.dev@gmail.com>
2020-08-07 18:05:18 +0700

    fix landing g04

---
+++ view/landing-g04/src/main.js
@@ -0,0 +1,26 @@
+$('.slider-wrapper').slick({
+  dots: true,
+  centerMode: true,
+  centerPadding: '11.667vw',
+  slidesToShow: 1,
+  // autoplay: true,
+  // autoplaySpeed: 4000,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        arrows: false,
+        centerPadding: '18.75vw',
+      }
+    }
+  ]
+});
+
+$('.back-top').click(function () {
+  $('body, html').animate({
+      scrollTop: 0
+  }, 1000);
+});
+
+
+
