7c16cebb7828d77f8b109a281a318d7cc523fcd3
anhnx <anhnx.dev@gmail.com>
2020-09-25 11:19:07 +0700

    Up loading of gzone

--- view/loading-gzone/loading-01.js
+++ view/loading-gzone/loading-01.js
@@ -2,26 +2,42 @@ function star_loading() {
   $(document).ready(function () {
     $('body').prepend(`
       <div class="c-loading">
-        <div class="over"></div>
-        <div class="loading"><span class="wrapper"><img src="./logo-gzone.png"></span></div>
+        <div class="over-black"></div>
+        <div class="loading">
+          <span class="wrapper">
+            <img src="./logo-gzone.png">
+            <div class="dots">
+              <div class="dot"></div>
+              <div class="dot"></div>
+              <div class="dot"></div>
+          </div>
+          </span>
+        </div>
       </div>
     `);
   });
 }
 
-
-function no_over_loading() {
-  $('.c-loading .over').remove();
+function star_loadingIPA() {
+  $(document).ready(function () {
+    $('body').prepend(`
+      <div class="c-loading">
+        <div class="over-dark"></div>
+        <div class="loading">
+          <span class="wrapper">
+            <img src="./logo-gzone.png">
+            <div class="dots">
+              <div class="dot"></div>
+              <div class="dot"></div>
+              <div class="dot"></div>
+          </div>
+          </span>
+        </div>
+      </div>
+    `);
+  });
 }
 
 function end_loading() {
   $('.c-loading').remove();
 }
-
-// star_loading();
-
-// setTimeout(function () {
-
-//   end_loading();
-
-// }, 5000);
