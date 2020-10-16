e65ee25771a17d76806469598b282623d1789f3e
Anh Nguyen <anhnx26121992@gmail.com>
2020-10-16 15:53:07 +0700

    Update education-doan and hi-game

--- view/education/src/main.js
+++ view/education/src/main.js
@@ -9,4 +9,38 @@ btnUpdateForm.on('click', function () {
 btnSaveForm.on('click', function () {
   $(this).parent().parent().removeClass('active');
 })
+
+// switch
+const BTNswitch = $('.switch');
+BTNswitch.on('click', function () {
+  $(this).toggleClass('active');
+})
+
+// more school calendar
+const moreSchoolCalendar = $('.c-more_school_calendar');
+if (moreSchoolCalendar) {
+
+  const btnOpen = $('.new-alarm');
+  const btnExit = $('.calendar .btn-exit');
+  const calendar = $('.calendar');
+
+  BTNswitch.on('click', function () {
+    $(this).parent().toggleClass('active');
+  });
+
+  btnOpen.on('click', function () {
+    calendar.addClass('active');
+  });
+  btnExit.on('click', function () {
+    calendar.removeClass('active');
+  });
+}
+
+
+// Game
+const btnAnswer_01 = $('.js-answer-v1 .answer');
+btnAnswer_01.on('click', function () {
+  $(this).toggleClass('active').siblings().removeClass('active');
+})
+
   
